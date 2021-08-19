8d1cb0778111d5b8509fb2c062d561c1f54650cf
932445 <2458449883@qq.com>
2021-08-20 01:30:32 +0800

    1

--- src/utils/Wallet.js
+++ src/utils/Wallet.js
@@ -23,7 +23,7 @@ const PrecisionsObj = [
 const tokensContractAddress = "0x5F8Bc83B6285A495c13347130352E9f20cebA63D";
 // const _contractAddress = '0x7ac13B3aEe65616eb16729Da45D8204E8871Fce0';
 //币安测试链
-const _contractAddress = '0x3053964720b88424a83A0D0113a7CAAaB40cE1A6';
+const _contractAddress = '0xCc400211B960CA1f6DE7fd03306eD159a57f245f';
 const _contractABI = [
     {
         "inputs": [],
@@ -2938,7 +2938,7 @@ function takeoutOne(account,currency,amount, callback, errorCallback){
     const data = _contract.methods
         .takeoutOne(
             getCurrencyIndex(currency),
-            amount
+            Number(amount)*systemPrecisions,
         ).encodeABI();
     sendTransfer(account, data, 0x0, callback, errorCallback);
 }
@@ -2948,7 +2948,7 @@ function takeoutTwo(account,amount1,currency2,callback, errorCallback){
     const _contract = new window.web3.eth.Contract(_contractABI, _contractAddress);
     const data = _contract.methods
         .takeoutTwo(
-            amount1,
+            Number(amount1)*systemPrecisions,
             getCurrencyIndex(currency2),
         ).encodeABI();
     sendTransfer(account, data, 0x0, callback, errorCallback);
