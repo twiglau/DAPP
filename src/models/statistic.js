61a9a4689cc63db3a0201608744f9ffc54f7fe5e
lau <twigliu@hotmail.com>
2021-09-06 11:10:24 +0800

    feat:统计数据

---
+++ src/models/statistic.js
@@ -0,0 +1,68 @@
+
+import Wallet from '@/utils/Wallet.js';
+import {currencyMap} from '@/utils/api';
+function Statistic(){
+   this.coins = currencyMap;
+   this.totalLock = 0;
+   this.destoryAmount = 0;
+   this.feeAmount = 0;
+}
+
+
+    //计算总锁仓
+Statistic.prototype.getPlatformLockAmount = function(){
+    let _self = this
+    return new Promise((resolve,reject) => {
+        try {
+            let promiseAllarr = []
+            for(let i = 0; i < _self.coins.length;i++){
+            console.log(i)
+            promiseAllarr[i] = new Promise((res1,rej1) => {
+                    Wallet.totalUseableBalance(_self.coins[i].currency,(in_a)=>{
+                    let val = +in_a
+                    in_a > 0 && (val = Number(in_a / Wallet.Precisions()))
+                    val < -0.01 && (val = 0)
+                    res1(val)
+                    },(err) =>{rej1(err)})
+                })
+            }
+            //全部请求
+            Promise.all(promiseAllarr).then((res) => {
+            resolve(res)
+            }).catch(err => {
+            reject(err)
+            })
+        }catch(err){
+            reject(err)
+        }
+    })
+}
+
+Statistic.prototype.calculateTeamPerformance = function(){
+    let _self = this;
+    if(_self.coins[1].price < 3){
+       const res = await _self.getCoinsPrice();
+       //更新价格
+       res.forEach((ele,index) => {
+         _self.coins[index].price = (+ele || 1)
+       })
+    }
+    var totala = 0;
+    for(let i = 0,len = _self.coins.length; i < len; i++){
+      let item = _self.coins[i]
+      totala += item.price * item.amount
+    }
+    _self.totalLock = totala
+    
+}
+Statistic.prototype.getCoinsPrice = function(){
+    let _self = this
+    let promiseCoinRequestArray = this.coins.map(ele => {
+        return new Promise((resolve,reject) => {
+             Wallet.queryPrice(ele.currency.toLowerCase(),(res)=>{
+                resolve(Number(res? res : 1))
+             },err => reject(err))
+        })
+    })
+    return Promise.all(promiseCoinRequestArray)
+}
